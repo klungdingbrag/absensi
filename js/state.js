67ecb626291d0de0016d51fe64e43cbb1ac77152
klungdingbrag <cimunmoses92@gmail.com>
2026-08-12 16:01:35 +0700

    Add fail-safe cloud state and local backup tracking

--- js/state.js
+++ js/state.js
@@ -1,3 +1,7 @@
 let dataKaryawan = [];
 let timerSave = null;
 let loadingWeek = false;
+let loadedWeekKey = null;
+let cloudReadyForWeek = false;
+let pendingSave = false;
+const LOCAL_BACKUP_PREFIX = "tb_nusantara_absensi_backup_";
